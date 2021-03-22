8d8d7806cf4763c84f5f16d0af516b9b2ec617cd
testor <stathell@yandex.ru>
2021-03-22 22:56:53 +0100

    commit 20210322

--- chapter1/code/protocol.py
+++ chapter1/code/protocol.py
@@ -1,14 +1,15 @@
 class protocol(object):
 
-	def __init__(self, name, number,description):
-		self.name = name
-		self.number = number
-		self.description = description
+    def __init__(self, name, number, description):
+        self.name = name
+        self.number = number
+        self.description = description
 
-	def getProtocolInfo(self):
-		return self.name+ " "+str(self.number)+ " "+self.description
-		
-protocol_http= protocol("HTTP", 80, "Hypertext transfer protocol")
+    def getProtocolInfo(self):
+        return self.name + " " + str(self.number) + " " + self.description
+
+
+protocol_http = protocol("HTTP", 80, "Hypertext transfer protocol")
 
 print(protocol_http.name)
 print(protocol_http.number)
